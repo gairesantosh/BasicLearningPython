0faaedc3e59f42f1ee47d10aec1f15a9daf7a722
Santosh Gaire <126267806+gairesantosh@users.noreply.github.com>
2025-02-06 20:55:30 +0545

    Add files via upload
    Break And Continue statement in FOR Looping statement

---
+++ ForLoopContinueBreakStatement.py
@@ -0,0 +1,14 @@
+#continue and break in looping statement
+#continue bhaneko "skip" break bhaneko "exit the program"
+
+#FOR LOOP_continue
+for j in range (50):
+    if (j==10):
+        continue
+    print (j)
+    
+ #FOR LOOP_break  
+for x in range (20):
+    if (x==10):
+        break
+    print(x)
