e2c3e0b1035f65a97798143b7802fc67a2370393
Santosh Gaire <126267806+gairesantosh@users.noreply.github.com>
2025-02-08 01:13:21 +0545

    Add files via upload
    learn function

---
+++ FunctionInPython.py
@@ -0,0 +1,16 @@
+'''
+Function
+'''
+
+def calculateArea (l,b):
+    a = l * b
+    return a 
+    
+print (f'area is {calculateArea (10,5)}')
+    
+    
+def calculateVol (l,b,h):
+    v = l * b * h
+    return v
+
+print (f'volume is {calculateVol(10,4,2)}')
